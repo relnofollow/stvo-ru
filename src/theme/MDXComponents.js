f05cb4aa393aaafe49d0e1dbcdb22137a8288833
Elena Kim <elena.a.zhdanova@gmail.com>
2024-10-16 22:50:21 +0200

    fix: Adjust translations for traffic signs (part I)

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -2,10 +2,12 @@ import MDXComponents from '@theme-original/MDXComponents';
 import VerifiedTranslationIcon from '@site/src/components/VerifiedTranslationIcon';
 import TrafficSign from '@site/src/components/TrafficSign';
 import MdTab from '../components/MarkdownTab';
+import Figure from '../components/Figure';
 
 export default {
   ...MDXComponents,
-  VerifiedTranslationIcon,
+  MdTab,
   TrafficSign,
-  MdTab
+  VerifiedTranslationIcon,
+  Figure
 };
