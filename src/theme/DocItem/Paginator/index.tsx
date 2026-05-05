34587fef9740233f56e2e2bfa5d9f2b84e9c84de
Elena Kim <elena.a.zhdanova@gmail.com>
2026-05-05 10:18:52 +0200

    feat: remove 'buy me a coffee' link

--- src/theme/DocItem/Paginator/index.tsx
+++ src/theme/DocItem/Paginator/index.tsx
@@ -1,7 +1,6 @@
 import React, { type ReactNode, useEffect } from "react";
 import { useDoc } from "@docusaurus/plugin-content-docs/client";
 import DocPaginator from "@theme/DocPaginator";
-import Tip from "@theme-original/Admonition/Type/Tip";
 import Info from "@theme-original/Admonition/Type/Info";
 
 /**
@@ -22,20 +21,6 @@ export default function DocItemPaginator(): ReactNode {
       >
         Вы можете <a href="/download">скачать перевод в PDF</a>.
       </Info>
-      <Tip title="Поддержать проект" className={"margin-top--lg"} icon="🙏">
-        <p>
-          Если перевод оказался для вас полезным и вы хотите поддержать проект:
-        </p>
-        <p>
-          <a href="https://www.buymeacoffee.com/stvo.ru" target="_blank">
-            <img
-              src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
-              alt="Buy Me A Coffee"
-              style={{ height: "40px", width: "145px" }}
-            />
-          </a>
-        </p>
-      </Tip>
     </>
   );
 }
